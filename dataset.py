f12933f52330783aa25b57b906d5594e1e050b5a
Doan Bao Ngan Le <ldbaongan.2701@gmail.com>
2024-03-10 13:15:15 -0400

    Add cv_random_flip as data augmentation method
    The augmentation method will randomly flip the image along the width dimension.
    
    This add noise to the dataset for better model performance

--- dataset.py
+++ dataset.py
@@ -3,6 +3,8 @@ import numpy as np
 import cv2
 import random
 from PIL import Image
+import torch
+from torch.utils import data
 
 def load_image(path):
     """Load an image and apply preprocessing steps to transform image into suitable format.
@@ -64,3 +66,27 @@ def load_sal_label(path):
     label = label / 255.                        # Pixel Normalization - scale pixel value to a range between 0-1
     label = label[np.newaxis, ...]              # Thresholding - convert pixel to binary values (0 or 1). Thus convert image to binary representation
     return label                                # Add a new dimension at the beginning of the array to make it a 1 x height x width array (required by the loss) 
+
+
+def cv_random_flip(img, label, edge):
+    """
+    Data Augmentation -  randomly decides whether to flip the input images (img, label, and edge) along the Width dimension 
+    and performs the flip operation on all three images if the random flip flag is set to 1
+
+    Args:
+        img (np.array): orginal image
+        label (np.array): salient object i.e. label image
+        edge (np.array): salient edge image
+
+    Returns:
+        Tuple of 3 np.array: A tuple containing the flipped versions of the original image, label image, and edge image.
+    """
+    flip_flag = random.randint(0, 1)
+    
+    # [:, :, ::-1] - select all elements along the channels dimension (first :) and height dimension (second :)
+    # and reverse the order of elements along the width dimension (last ::-1) 
+    if flip_flag == 1:                       
+        img = img[:,:,::-1].copy()
+        label = label[:,:,::-1].copy()
+        edge = edge[:,:,::-1].copy()
+    return img, label, edge
